de0343c42fda8f1e932e4a020fe833318de71c34
Anthony Louis <anthony.physis@gmail.com>
2018-08-01 16:51:13 -0300

    create sayhelloworld

--- src/main/java/BhabakaBot.java
+++ src/main/java/BhabakaBot.java
@@ -1,4 +1,7 @@
 import org.telegram.abilitybots.api.bot.AbilityBot;
+import org.telegram.abilitybots.api.objects.Ability;
+import org.telegram.abilitybots.api.objects.Locality;
+import org.telegram.abilitybots.api.objects.Privacy;
 
 public class BhabakaBot extends AbilityBot {
     public static final String BOT_TOKEN = "671350952:AAEE-HLESD7QuesAebCWthFZFWLobh4o7VQ";
@@ -10,5 +13,17 @@ public class BhabakaBot extends AbilityBot {
 
     public int creatorId() {
         return 123456789;
+    }
+
+    // adicionando habilidade para o bot dizer hello
+    public Ability sayHelloWorld() {
+        return Ability
+                .builder()
+                .name("hello")
+                .info("says hello world")
+                .locality(Locality.ALL)
+                .privacy(Privacy.PUBLIC)
+                .action(ctx -> silent.send("hello world!", ctx.chatId()))
+                .build();
     }
 }
