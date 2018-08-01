4feb3176870e800b1f605a59268cb352c01e9da9
Anthony Louis <anthony.physis@gmail.com>
2018-08-01 17:01:17 -0300

    creating application

---
+++ src/main/java/Application.java
@@ -0,0 +1,20 @@
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class Application {
+    public static void main(String args[]) {
+        //Initialize dependencies necessary for the base dot - Guice
+        ApiContextInitializer.init();
+
+        // Create the TelegramBotsApi object to register your bots
+        var botsApi = new TelegramBotsApi();
+
+        try {
+            // Register your newly created AbilityBot
+            botsApi.registerBot(new BhabakaBot());
+        } catch(TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+}
